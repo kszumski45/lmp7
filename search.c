80f8af3c9af8f60e5d60103a29ba497407feaf25
Karol Szumski <k.szumski45@gmail.com>
2015-11-20 02:54:00 +0100

    dodano search.c i search.h

---
+++ search.c
@@ -0,0 +1,8 @@
+#include <ctype.h>
+#include "search.h"
+
+
+char * search(char* source, char*str)
+{
+			
+}
---
+++ search.h
@@ -0,0 +1,6 @@
+#ifndef _SEARCH_H
+#define _SEARCH_H
+
+char* search(char* source, char* str);
+
+#endif
